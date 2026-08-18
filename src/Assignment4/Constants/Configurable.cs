445a186a6a2cefa5986276fd10977be9a138f28b
27029-Dharun <dharun.ramasamy@solitontech.com>
2026-08-18 09:35:27 +0530

    fix: minimum amount constraint

--- src/Assignment4/Constants/Configurable.cs
+++ src/Assignment4/Constants/Configurable.cs
@@ -29,6 +29,6 @@
         /// <summary>
         /// Represents the minimum amount that can be tracked.
         /// </summary>
-        public const int MinimumAmount = 0;
+        public const int MinimumAmount = 1;
     }
 }
