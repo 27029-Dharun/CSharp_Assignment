3e3d7c5357d69979fe4699a1e1c432eaabf59f45
27029-Dharun <dharun.ramasamy@solitontech.com>
2026-08-18 14:49:22 +0530

    feat: appdomain

--- src/Assignment8/Helpers/ConsoleHelper.cs
+++ src/Assignment8/Helpers/ConsoleHelper.cs
@@ -2,8 +2,6 @@
 {
     internal static class ConsoleHelper
     {
-        private const int Tries = 3;
-
         public static void PrintColoredText(string message, ConsoleColor color)
         {
             Console.ForegroundColor = color;
@@ -15,57 +13,6 @@
         {
             Console.Write(prompt);
             string input = (Console.ReadLine() ?? string.Empty).Trim();
-            return input;
-        }
-
-        public static T GetEnumValue<T>(string message)
-    where T : struct, Enum
-        {
-            int tries = Tries;
-            Console.WriteLine($"\n{message}");
-            foreach (var value in Enum.GetValues<T>())
-            {
-                Console.WriteLine($"{Convert.ToInt32(value)}. {value}");
-            }
-
-            string input = Console.ReadLine() ?? string.Empty;
-            int integer;
-            while (!int.TryParse(input, out integer) || !Enum.IsDefined(typeof(T), integer))
-            {
-                if (tries == 1)
-                {
-                    throw new InvalidDataException("No attempt left, Please try again." + Environment.NewLine);
-                }
-
-                Console.WriteLine($"Tries left: {--tries}, Enter the valid integer");
-                Console.WriteLine($"{message}");
-                input = Console.ReadLine() ?? string.Empty;
-            }
-
-            return (T)Enum.ToObject(typeof(T), integer);
-        }
-
-        public static string GetValidatedInput(string prompt, bool optional, Func<string, bool> isValidField, string errorMessage)
-        {
-            int tries = Tries;
-            string input = GetString(prompt);
-            if (optional && string.IsNullOrWhiteSpace(input))
-            {
-                return string.Empty;
-            }
-
-            while (!isValidField(input))
-            {
-                if (tries == 1)
-                {
-                    throw new InvalidDataException("No attempt left, Please try again." + Environment.NewLine);
-                }
-
-                Console.WriteLine(errorMessage);
-                Console.WriteLine($"Tries left: {--tries}\n");
-                input = GetString(prompt);
-            }
-
             return input;
         }
     }
--- src/Assignment8/Program.cs
+++ src/Assignment8/Program.cs
@@ -7,9 +7,18 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        AppDomain domain = AppDomain.CurrentDomain;
+        domain.UnhandledException += new UnhandledExceptionEventHandler(HandleException);
+
         ConsoleView view = new ConsoleView();
         Controller controller = new Controller(view);
 
         controller.HandleMenu();
+        Console.ReadKey();
+    }
+
+    public static void HandleException()
+    {
+        Console.WriteLine($"Unhandled exception caught, {}");
     }
 }
