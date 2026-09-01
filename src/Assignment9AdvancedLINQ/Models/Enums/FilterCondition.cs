7e97a7408a03bf5e45cf15f93f989c2ef70dce39
27029-Dharun <dharun.ramasamy@solitontech.com>
2026-09-01 09:52:22 +0530

    feat: bonus task

---
+++ src/Assignment9AdvancedLINQ/Models/Enums/FilterCondition.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment9AdvancedLINQ.Models.Enums
+{
+    /// <summary>
+    /// Specifies the options available to perform filter
+    /// </summary>
+    public enum FilterCondition
+    {
+        /// <summary>
+        /// Represents the option to filter the collection greater than or equal operation.
+        /// </summary>
+        GreaterThanOrEqualTo,
+
+        /// <summary>
+        /// Represents the option to filter the collection less than or equal operation.
+        /// </summary>
+        LessThanOrEqualTo,
+
+        /// <summary>
+        /// Represents the option to filter the collection by checking it has the giving characters.
+        /// </summary>
+        Contains,
+
+        /// <summary>
+        /// Represents the option to filter the entries with starting characters.
+        /// </summary>
+        StartsWith,
+
+        /// <summary>
+        /// Represents the option to filter the entries with ending characters.
+        /// </summary>
+        EndsWith,
+    }
+}
