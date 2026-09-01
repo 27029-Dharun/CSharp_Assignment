2ca369a90bddcae11912fb6f440e9949abfd09c0
27029-Dharun <dharun.ramasamy@solitontech.com>
2026-09-01 11:08:07 +0530

    feat: init command

---
+++ src/Assignment10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignments
+{
+    /// <summary>
+    /// Program class which acts as the start of the program and calls the respective tasks.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main method of the program.
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
