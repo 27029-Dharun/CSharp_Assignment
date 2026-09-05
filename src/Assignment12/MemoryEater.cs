74c792dd9f5958ce3874e722c20642ed4ad4ecd9
27029-Dharun <dharun.ramasamy@solitontech.com>
2026-09-05 16:48:34 +0530

    feat: add memoryEater

---
+++ src/Assignment12/MemoryEater.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment12;
+
+/// <summary>
+/// Contains logic to allocate memory
+/// </summary>
+internal class MemoryEater
+{
+    /// <summary>
+    /// Allocates memory and adds it to a list
+    /// </summary>
+    public void Allocate()
+    {
+        while (true)
+        {
+            List<int[]> memAlloc = new List<int[]>();
+            memAlloc.Add(new int[1000]);
+        }
+    }
+}
---
+++ src/Assignment12/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment12;
+
+/// <summary>
+/// Application entry point.
+/// </summary>
+internal class Program
+{
+    private static void Main()
+    {
+        MemoryEater memoryEater = new MemoryEater();
+        memoryEater.Allocate();
+        Console.ReadKey();
+    }
+}
