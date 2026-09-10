bf5da4827a3e342e0739458e593bd522f4720f85
27029-Dharun <dharun.ramasamy@solitontech.com>
2026-09-10 09:26:34 +0530

    feat: add task 2

--- src/Collections/Tasks/StackOperations.cs
+++ src/Collections/Tasks/StackOperations.cs
@@ -2,13 +2,29 @@
 
 namespace Collections.Tasks;
 
-internal class StackOperations
+/// <summary>
+/// Contains stack implementation.
+/// </summary>
+public class StackOperations
 {
-    Stack<char> chars = new Stack<char>();
+    private readonly Stack<char> _chars = new Stack<char>();
 
+    /// <summary>
+    /// Gets a string input and load the characters in string into the stack.
+    /// </summary>
     public void AddCharacter()
     {
-        string c = ConsoleIO.GetString("Enter a string: ");
-        this.chars.Push(c[0]);
+        string input = ConsoleIO.GetString("Enter a string: ");
+        foreach (char character in input)
+        {
+            this._chars.Push(character);
+        }
+    }
+
+    /// <summary>
+    /// Pops all the characters and append to a new string.
+    /// </summary>
+    public void RemoveCharacter()
+    {
     }
 }
